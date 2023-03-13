7b2d43217b716bdfcb8182ee83712f5822c907e7
Rémi Tran <tranremi@live.fr>
2023-03-13 14:35:50 +0100

    Authorization > error handling

--- src/GoogleIdentity/authorization.js
+++ src/GoogleIdentity/authorization.js
@@ -3,20 +3,32 @@ import {
   clientLoaded
 } from './client'
 
+// NonOAuthError
+const error_callback = (err) => {
+  if (err.type == 'Non-OAuth Error: popup_failed_to_open') {
+    console.warn('The popup window is failed to open');
+  } else if (err.type == 'popup_closed') {
+    console.warn('Non-OAuth Error: The popup window is closed before an OAuth response is returned');
+  }
+};
+
+// # Implicit flow
 export const requestAccessToken = () => {
   return new Promise((resolve, reject) => {
     clientLoaded((google) => {
       google.accounts.oauth2
         .initTokenClient({
+          ...state.authorizationConfiguration,
           client_id: state.clientId,
-          scope: state.authorizationConfiguration.scope,
           callback: (response) => {
-            if (response.access_token) {
-              resolve(response);
-            } else {
+            if (response.error) {
+              console.warn(`OAuth Error: ${response.error}`);
               reject(response);
+            } else if (response.access_token) {
+              resolve(response);
             }
           },
+          error_callback,
         })
         .requestAccessToken();
     });
@@ -29,16 +41,18 @@ export const requestCode = () => {
     clientLoaded((google) => {
       google.accounts.oauth2
         .initCodeClient({
+          ...state.authorizationConfiguration,
           client_id: state.clientId,
-          scope: state.authorizationConfiguration.scope,
           ux_mode: 'popup',
           callback: (response) => {
-            if (response.code) {
-              resolve(response);
-            } else {
+            if (response.error) {
+              console.warn(`OAuth Error: ${response.error}`);
               reject(response);
+            } else if (response.code) {
+              resolve(response);
             }
           },
+          error_callback,
         })
         .requestCode();
     });
